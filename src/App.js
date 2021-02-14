31dde55f31c313f38ec808f0843cb2ca19beb616
maxmccomb <maxwellmccomb16@augustana.edu>
2021-02-14 14:55:20 -0600

    added code for API call locally

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import {Button, Col, Row, Container, Spinner} from 'react-bootstrap';
+import {useState} from 'react';
+import axios from 'axios';
 
 function App() {
+
+
+const handleWaterNowButton = () => {
+    axios.get('http://localhost:8080/waterNow')
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+        console.error('There was an error!', error);
+    });
+}
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div>
+          <br/>
+          <br/>
+          <Button size="lg" block onClick={handleWaterNowButton}>Water Now</Button>
+        </div>
     </div>
   );
 }
